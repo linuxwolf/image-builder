f387e55384e69943215b95cb665582dbab3ab17e
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-07-20 16:49:30 -0600

    feat: caching roll-over and no provenance (#2)

--- image.mk
+++ image.mk
@@ -5,7 +5,9 @@ help:
 	@echo "init-builder .................... intialize building context"
 	@echo "  clean-builder ................. clean building context"
 	@echo "  clean-cache ................... clean cache"
-	
+
+DOCKER_CACHE_NEW=$(DOCKER_CACHE)-new
+
 init-builder: $(DOCKER_CACHE)
 	DOCKER_BUILDER=$(DOCKER_BUILDER) \
 	DOCKER_REGISTRY=$(DOCKER_REGISTRY) \
@@ -17,20 +19,26 @@ clean-builder:
 $(DOCKER_CACHE):
 	mkdir -p $(DOCKER_CACHE)
 
+$(DOCKER_CACHE_NEW):
+	mkdir -p $(DOCKER_CACHE_NEW)
+
 clean-cache:
-	rm -rf $(DOCKER_CACHE)
+	rm -rf $(DOCKER_CACHE) $(DOCKER_CACHE_NEW)
 
 $(DOCKER_REPO_OWNER)/%: STAMP?=$(shell git rev-parse --verify HEAD || echo latest)
 $(DOCKER_REPO_OWNER)/%: IMAGE_TAG?=$(STAMP)
-$(DOCKER_REPO_OWNER)/%: $(DOCKER_CACHE)
+$(DOCKER_REPO_OWNER)/%: $(DOCKER_CACHE) $(DOCKER_CACHE_NEW)
 	$(MAKE) init-builder && \
 	docker buildx build --builder $(DOCKER_BUILDER) \
+		--provenance=false \
 		--cache-from type=local,src=$(DOCKER_CACHE) \
-		--cache-to type=local,dest=$(DOCKER_CACHE),mode=max \
+		--cache-to type=local,dest=$(DOCKER_CACHE_NEW),mode=max \
 		--output type=image,push=true \
 		--platform linux/amd64,linux/arm64 \
 		--tag $(DOCKER_REGISTRY)/$@:$(IMAGE_TAG) \
 		--build-arg DOCKER_REGISTRY=$(DOCKER_REGISTRY) \
 		--build-arg STAMP=$(STAMP) \
 		-f $(CURDIR)/Dockerfile \
-		$(CURDIR)
+		$(CURDIR) && \
+		rm -rf $(DOCKER_CACHE) && \
+		mv $(DOCKER_CACHE_NEW) $(DOCKER_CACHE)
